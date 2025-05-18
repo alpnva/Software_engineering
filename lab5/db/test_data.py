de56cd636d4cb776fc5cd074ebb98f64424d8938
alpnva <74810136+alpnva@users.noreply.github.com>
2025-05-18 12:35:45 +0300

    Add files via upload

---
+++ lab5/db/test_data.py
@@ -0,0 +1,78 @@
+USERS_DATA = [
+    {
+        "username": "Иван Иванов",
+        "role": "admin",
+        "login": "admin",
+        "password": "secret"
+    },
+    {
+        "username": "Анна Кузнецова",
+        "role": "user",
+        "login": "anna_22",
+        "password": "anna_22"
+    },
+    {
+        "username": "Сергей Белкин",
+        "role": "user",
+        "login": "sergey_b",
+        "password": "sergey2024"
+    },
+    {
+        "username": "Мария Лебедева",
+        "role": "user",
+        "login": "maria_leb",
+        "password": "password123"
+    },
+    {
+        "username": "Дмитрий Орлов",
+        "role": "user",
+        "login": "orlov_d",
+        "password": "orlovpass"
+    }
+]
+
+INCOMES_DATA = [
+    {
+        "amount": 120000.00,
+        "description": "Зарплата за март",
+        "user_login": "anna_22"
+    },
+    {
+        "amount": 5000.00,
+        "description": "Фриланс проект",
+        "user_login": "sergey_b"
+    },
+    {
+        "amount": 80000.00,
+        "description": "Зарплата",
+        "user_login": "maria_leb"
+    },
+    {
+        "amount": 15000.00,
+        "description": "Сдача квартиры",
+        "user_login": "orlov_d"
+    }
+]
+
+EXPENSES_DATA = [
+    {
+        "amount": 35000.00,
+        "description": "Аренда жилья",
+        "user_login": "anna_22"
+    },
+    {
+        "amount": 8000.00,
+        "description": "Коммунальные услуги",
+        "user_login": "sergey_b"
+    },
+    {
+        "amount": 10000.00,
+        "description": "Продукты",
+        "user_login": "maria_leb"
+    },
+    {
+        "amount": 5000.00,
+        "description": "Интернет и связь",
+        "user_login": "orlov_d"
+    }
+]
